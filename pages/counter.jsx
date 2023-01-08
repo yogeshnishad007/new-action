8250fd638882f09e25861f00a530cb310cc13512
yogeshnishad007 <yogiatoy27518@gmail.com>
2023-01-09 00:13:55 +0530

    action file

---
+++ pages/counter.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [count,setCount]=useState(0)
+  return (
+    <div>
+        
+         <h1>
+         {count}
+    
+         </h1>
+    
+
+         <div>
+            <button onClick={()=> setCount(count-1)}>-</button>
+            <button onClick={()=> setCount(count+1)}>+</button>
+         </div>
+    
+    </div>
+
+   
+  )
+}
+
+export default Counter;
